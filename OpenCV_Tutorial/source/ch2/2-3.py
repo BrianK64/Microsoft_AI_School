aa942df510565139c2dfa9add8a443eb7bc9562a
BrianK64 <kmsskyblue@gmail.com>
2025-02-10 17:22:36 +0900

    Gray scale image and resized image

--- OpenCV_Tutorial/source/ch2/2-3.py
+++ OpenCV_Tutorial/source/ch2/2-3.py
@@ -1,1 +1,20 @@
-    
+import cv2 as cv
+import sys
+
+img = cv.imread("OpenCV_Tutorial\source\ch2\soccer.jpg")
+
+if img is None:
+    sys.exit("Fild not found")
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+gray_small = cv.resize(gray, dsize = (0, 0), fx = 0.5, fy = 0.5)
+
+cv.imwrite("OpenCV_Tutorial\source\ch2\soccer_gray.jpg", gray)
+cv.imwrite("OpenCV_Tutorial\source\ch2\soccer_gray_small.jpg", gray_small)
+
+cv.imshow("Color image", img)
+cv.imshow("Gray image", gray)
+cv.imshow("Gray image small", gray_small)
+
+cv.waitKey()
+cv.destroyAllWindows()
