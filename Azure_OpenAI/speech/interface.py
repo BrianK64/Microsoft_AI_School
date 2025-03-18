af4be29819e20d8e884791f5911211e721d5c30c
BrianK64 <kmsskyblue@gmail.com>
2025-03-18 12:40:21 +0900

    microphone input support

--- Azure_OpenAI/speech/interface.py
+++ Azure_OpenAI/speech/interface.py
@@ -8,3 +8,10 @@ def main():
             gr.Markdown("## Speech to Text")
 
             input_mic = gr.Audio(label="Mic", sources = "microphone", type = "filepath", show_download_button = True)
+            output_textbox = gr.Textbox(label = "Transcribed Text", interactive = False)
+
+    page_main.launch()
+
+
+if __name__ == "__main__":
+    main()
