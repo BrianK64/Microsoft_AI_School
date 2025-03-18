b76f6190fc95c68f678628f1402619c3e203fa67
BrianK64 <kmsskyblue@gmail.com>
2025-03-18 12:32:54 +0900

    initial interface setup

--- Azure_OpenAI/speech/interface.py
+++ Azure_OpenAI/speech/interface.py
@@ -1,1 +1,10 @@
 import gradio as gr
+
+def main():
+    with gr.Blocks() as page_main:
+        gr.Markdown("# Azure AI Speech Service")
+
+        with gr.Column():
+            gr.Markdown("## Speech to Text")
+
+            input_mic = 
