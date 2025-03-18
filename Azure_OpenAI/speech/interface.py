bb63d019d01b96d5d4df8fda76e28bfcc9b5e78f
BrianK64 <kmsskyblue@gmail.com>
2025-03-18 12:44:42 +0900

    audio source

--- Azure_OpenAI/speech/interface.py
+++ Azure_OpenAI/speech/interface.py
@@ -4,11 +4,15 @@ def main():
     with gr.Blocks() as page_main:
         gr.Markdown("# Azure AI Speech Service")
 
+        def update_audio_source(audio_path):
+            return audio_path
+
         with gr.Column():
             gr.Markdown("## Speech to Text")
 
             input_mic = gr.Audio(label="Mic", sources = "microphone", type = "filepath", show_download_button = True)
-            output_textbox = gr.Textbox(label = "Transcribed Text", interactive = False)
+            transcription_textbox = gr.Textbox(label = "Transcribed Text", interactive = False)
+            input_mic.change(update_audio_source, inputs = [input_mic], outputs = [transcription_textbox])
 
     page_main.launch()
 
