7b52b463d3a4d730bf4c1d1c72f3df798a49f0b1
BrianK64 <kmsskyblue@gmail.com>
2025-03-21 15:17:24 +0900

    JARVIS: gpt-4o mini model functionality

--- Azure_OpenAI/main/agent.py
+++ Azure_OpenAI/main/agent.py
@@ -22,3 +22,56 @@ class Agent():
         # Text-to-speech credentials
         self.API_TEXT_TO_SPEECH_ENDPOINT = os.getenv("API_TEXT_TO_SPEECH_ENDPOINT")
         self.API_TEXT_TO_SPEECH_KEY = os.getenv("API_TEXT_TO_SPEECH_KEY")
+
+    def gpt_4o_mini(self, prompt):
+        
+        headers = {
+            "Content-Type": "application/json",
+            "api-key": self.API_OPENAI_KEY
+        }
+
+        body = {
+            "messages": [
+                {
+                    "role": "system",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "You are Jarvis, an AI assistant that helps people find information, perform daily tasks, and provide insights. You will always answer in a very professional and formal tone, and always greet the user with 'Hello, sir'"
+                        }
+                    ]
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": prompt
+                        }
+                    ]
+                }
+            ],
+            "temperature": 0.7,
+            "top_p": 0.9,
+            "max_tokens": 2000
+        }
+
+        response = requests.post(self.API_OPENAI_ENDPOINT, headers = headers, json = body)
+
+        if response.status_code == 200:
+            response_json = response.json()
+            message = response_json["choices"][0]["message"]
+            role = message["role"]
+            content = message["content"]
+            return content
+        
+        else:
+            print(f"Error {response.status_code}: {response.reason}")
+            return None
+        
+
+if __name__ == "__main__":
+    agent = Agent()
+    prompt = "Hi Jarvis"
+    response = agent.gpt_4o_mini(prompt = prompt)
+    print(response)
