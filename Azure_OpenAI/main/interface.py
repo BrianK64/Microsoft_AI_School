a63dfdd93b9fe04458999b5e782a7fbef3e6a719
BrianK64 <kmsskyblue@gmail.com>
2025-03-21 16:37:13 +0900

    JARVIS: initial checkpoint

--- Azure_OpenAI/main/interface.py
+++ Azure_OpenAI/main/interface.py
@@ -1,23 +1,49 @@
 import gradio as gr
 from agent import Agent
 
-css = """
-body {
-    background-image: url('https://media.licdn.com/dms/image/v2/D5612AQE56Y6FyBgOCQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1724087970185?e=2147483647&v=beta&t=awk5enaARMlrqSzHQy8KQTn6qR2fCnCh0b7ztrmrQrc'); /* Replace with your image URL or path */
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-}
-"""
+
+agent = Agent()
+
+
+def activate():
+    return gr.update(visible=True), gr.update(visible=False)
+
+
+def set_audio(audio_path):
+    if audio_path:
+        text = agent.speech_to_text(audio_path)
+        return text
+    else:
+        return None
+
 
 def main():
     with gr.Blocks() as JARVIS:
+
+        with gr.Column(visible = True) as deactivated:
         
-        Jarvis = gr.Chatbot()
+            Jarvis_deactivated = gr.Image("Azure_OpenAI/main/jarvis_background.png", interactive = True)
+            activation_button = gr.Button("activate")
 
+            activation_button.click(fn = activate, inputs = [], outputs = [])
 
-    JARVIS.launch()
+        
+        with gr.Column(visible = False) as activated:
+            Jarvis_activated = gr.Chatbot(label = "J.A.R.V.I.S", type = "messages")
+            
+            
+            query_stt_audio = gr.Audio(sources = "microphone", type = "filepath", show_download_button = False)
+            query_stt_text = ""
+            #query_stt_audio.change(fn = set_audio, inputs = [query_stt_audio], outputs = [query_stt_text])
+            query_tts_text = gr.Textbox()
+            deactivation_button = gr.Button("deactivate")
+            
+
+        activation_button.click(fn = activate, inputs = [], outputs = [activated, deactivated])
+        deactivation_button.click(fn = activate, inputs = [], outputs = [deactivated, activated])
+
+    return JARVIS
 
 
 if __name__ == "__main__":
-    main()
+    main().launch()
