64f457c0e94b5b0f8255463bd20a2e6dfb1a9d28
BrianK64 <kmsskyblue@gmail.com>
2025-03-21 15:13:02 +0900

    JARVIS: Azure AI Services Speech Service authentication

--- Azure_OpenAI/main/agent.py
+++ Azure_OpenAI/main/agent.py
@@ -12,4 +12,13 @@ class Agent():
         load_dotenv()
 
         # Azure AI Service Authentication
-        
+        self.API_OPENAI_ENDPOINT = os.getenv("API_OPENAI_ENDPOINT")
+        self.API_OPENAI_KEY = os.getenv("API_OPENAI_KEY")
+
+        # speech-to-text credentials
+        self.API_SPEECH_TO_TEXT_ENDPOINT = os.getenv("API_SPEECH_TO_TEXT_ENDPOINT")
+        self.API_SPEECH_TO_TEXT_KEY = os.getenv("API_SPEECH_TO_TEXT_KEY")
+
+        # Text-to-speech credentials
+        self.API_TEXT_TO_SPEECH_ENDPOINT = os.getenv("API_TEXT_TO_SPEECH_ENDPOINT")
+        self.API_TEXT_TO_SPEECH_KEY = os.getenv("API_TEXT_TO_SPEECH_KEY")
