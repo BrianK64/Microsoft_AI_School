70bc621180847e37702f494049f53ba9b4a0ad76
BrianK64 <kmsskyblue@gmail.com>
2025-03-22 22:40:57 +0900

    JARVIS: stt mic reset

--- Azure_OpenAI/main/interface.py
+++ Azure_OpenAI/main/interface.py
@@ -90,7 +90,7 @@ def main():
 
         # J.A.R.V.I.S speech processing
         query_button.click(fn = activate, inputs = [], outputs = [stt_audio]) #js="""() => {const audioComp = document.querySelector('input[type="file"]'); if (audioComp) { audioComp.click(); } }""")
-        stt_audio.change(fn = set_audio, inputs = [stt_audio], outputs = [stt_response]).then(fn = deactivate, inputs = [], outputs = [stt_audio])
+        stt_audio.change(fn = set_audio, inputs = [stt_audio], outputs = [stt_response]).then(fn = deactivate, inputs = [], outputs = [stt_audio]).then(fn = set_audio, inputs = [], outputs = [stt_audio])
 
         # J.A.R.V.I.S activation and deactivation
         activation_button.click(fn = toggle, inputs = [], outputs = [loading, deactivated]).then(fn = get_greeting, inputs = [], outputs = [greeting_textbox]).then(fn = get_audio, inputs = [greeting_textbox], outputs = [tts_audio]).then(fn = load2, inputs = [], outputs = [activated, loading])
