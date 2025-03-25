092f3746cd2b3a320c97a2de4d5c5d12d2acc8bb
BrianK64 <kmsskyblue@gmail.com>
2025-03-25 10:18:07 +0900

    Computer Vision - Image Analysis: populating request URL

--- Azure_AI/vision/image_analysis.py
+++ Azure_AI/vision/image_analysis.py
@@ -3,8 +3,9 @@ import os
 import requests
 
 # Microsoft Azure AI Services - Computer Vision Service - Image Analysis
-class Image_Analysis():
+class ImageAnalysisClient():
     
+
     def __init__(self):
 
         # load environment variables
@@ -13,5 +14,23 @@ class Image_Analysis():
         # Authentication credentials
         self.API_VISION_ENDPOINT = os.getenv("API_VISION_ENDPOINT")
         self.API_VISION_KEY = os.getenv("API_VISION_KEY")
+
+        # Query Parameters
         self.API_VISION_VERSION = os.getenv("API_VISION_VERSION")
+
+
+    # Get the request URL from populated endpoint with query parameters
+    def get_endpoint(self, *features):
+        API_POPULATED_VISION_ENDPOINT = "{}imageanalysis:analyze?api-version={}&features={}".format(self.API_VISION_ENDPOINT, self.API_VISION_VERSION, ','.join(features))
+        return API_POPULATED_VISION_ENDPOINT
+    
+
+
+
+    def image_analysis(self, image, *features):
         
+        return
+
+
+if __name__ == "__main__":
+    client = ImageAnalysisClient()
