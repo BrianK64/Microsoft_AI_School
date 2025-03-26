dd89cc9f3cb1bf0d4d02392c8904849f929480c3
BrianK64 <kmsskyblue@gmail.com>
2025-03-26 11:38:55 +0900

    Refactoring authentication process for optimization and efficiency

--- Azure_AI/vision/custom_vision/custom_vision.py
+++ Azure_AI/vision/custom_vision/custom_vision.py
@@ -22,29 +22,78 @@ class CustomVisionClient():
         self.API_CUSTOM_VISION_PREDICTION_KEY = os.getenv("API_CUSTOM_VISION_PREDICTION_KEY")
         self.API_CUSTOM_VISION_PREDICTION_RESOURCE_ID = os.getenv("API_CUSTOM_VISION_PREDICTION_RESOURCE_ID")
 
+        self.trainer = None
+        self.predictor = None
+
+        # Authenticate internally upon class object creation.
+        self.authenticate()
+
     
     # Instantiate a training and prediction client with authentication credentials
     def authenticate(self):
+        """
+        Authenticate and set up the trainer and predictor.
+        This method can be called explicitly to re-authenticate if needed.
+        """
         training_credentials = ApiKeyCredentials(in_headers={"Training-key": self.API_CUSTOM_VISION_TRAINING_KEY})
-        trainer = CustomVisionTrainingClient(endpoint = self.API_CUSTOM_VISION_TRAINING_ENDPOINT, credentials = training_credentials)
-        prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": self.API_CUSTOM_VISION_PREDICTION_KEY})
-        predictor = CustomVisionPredictionClient(endpoint = self.API_CUSTOM_VISION_PREDICTION_ENDPOINT, credentials = prediction_credentials)
+        self.trainer = CustomVisionTrainingClient(endpoint = self.API_CUSTOM_VISION_TRAINING_ENDPOINT, credentials = training_credentials)
 
-        return trainer, predictor
+        prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": self.API_CUSTOM_VISION_PREDICTION_KEY})
+        self.predictor = CustomVisionPredictionClient(endpoint = self.API_CUSTOM_VISION_PREDICTION_ENDPOINT, credentials = prediction_credentials)
 
     
     def get_project(self):
-        trainer, predictor = self.authenticate()
 
         # get project model
-        for project in trainer.get_projects():
+        for project in self.trainer.get_projects():
             print("PROJECT: {} - {}".format(project.name, project.id))
 
         # get project domain
-        for domain in trainer.get_domains():                            # list of available domains
+        for domain in self.trainer.get_domains():                            # list of available domains
             print("DOMAIN: {} - {}".format(domain.name, domain.id))     # information about each domain
+
+
+    # Create a new Custom Vision project
+    def set_project(self, project_name, project_description):
+
+        project_id, domain_id = self.validate_project(project_name)
+
+        if domain_id:
+
+            # If project with the same project name already exists, retrieve the existing project information.
+            if project_id:
+                project = self.trainer.get_project(project_id = project_id)
+
+            # Create a new project by configuring project name, project description, and a domain.
+            else:
+                project = self.trainer.create_project(project_name, project_description, domain_id)
+
+        return project.name, project.id
+
+    
+    def validate_project(self, project_name):
+
+        project_id, domain_id = "", ""
+
+        for project in self.trainer.get_projects():
+            if project.name == project_name:
+                project_id = project.id
+                break
+        
+        for domain in self.trainer.get_domains():
+            if domain.type == "ObjectDetection" and domain.name == "General (compact)":
+                domain_id = domain.id
+                break
+
+        return project_id, domain_id
 
 
 if __name__ == "__main__":
     client = CustomVisionClient()
     client.get_project()
+
+    # Project Creation
+    project_name = "6a009-objectDetection-model"
+    project_description = "Custom Vision Object Detection Project"
+    project_name, project_id = client.set_project(project_name = project_name, project_description = project_description)
+    print(project_name, project_id)
